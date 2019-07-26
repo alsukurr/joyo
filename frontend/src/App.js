b098631c83326acc62aa6ab35c8a9dce234b7945
Alsu <akurbanova@sidlee.com>
2019-07-26 16:21:31 -0400

    store a new day

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,18 @@ import './css/style.css';
 import HomePage from './components/HomePage';
 import AddDay from './components/AddDay';
 
+const storeDay = ({title, description}) => {
+  console.log(title, description, 'ji');
+};
 
 const App = () => {
+  // set it back to home
   const [screen, setScreen] = useState('addDay');
 
   return (
     <div className="app">
       { screen === 'home' && <HomePage setScreen = {setScreen} /> }
-      { screen === 'addDay' && <AddDay />}
+      { screen === 'addDay' && <AddDay storeDay={storeDay} />}
     </div>
   );
 }
--- frontend/src/components/AddDay.js
+++ frontend/src/components/AddDay.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AddDay = () => {
+const AddDay = ({ storeDay }) => {
+  const [title,  setTitle] = useState('');
+  const [description,  setDescription] = useState('');
+
   const saveDay = () => {
-    console.log('hello');
+    storeDay({title, description});
   }
 
   return (
     <div className="addDay">
-      <h1 className="addDay__title">add a day</h1>
-      <input type="text"/>
-      <h2 className="addDay__subtitle">description</h2>
-      <textarea name=""></textarea>
+      <h1 className="addDay__title">add a day {title}</h1>
+      <input type="text" onChange={e => setTitle(e.target.value)}/>
+      <h2 className="addDay__subtitle">description {description}</h2>
+      <textarea name="" onChange={e => setDescription(e.target.value)}></textarea>
       <button className="addDay__btn" onClick={saveDay}>Save</button>
     </div>
   );
